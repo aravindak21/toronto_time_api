fa4b9f6546d79c9745b854126165900ad8fff491
Aravind <vishnuk727@gmail.com>
2024-11-28 11:01:56 -0500

    Created app.log for error logging from server

--- main.go
+++ main.go
@@ -3,13 +3,24 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 	"toronto_time_api/db"
 	"toronto_time_api/handlers"
 )
 
 func main() {
+	// Open a log file to store logs
+	logFile, err := os.OpenFile("app.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logFile.Close()
+
+	// Set log output to the log file
+	log.SetOutput(logFile)
+
 	// Initialize the database connection
-	err := db.InitDB()
+	err = db.InitDB()
 	if err != nil {
 		log.Fatalf("Error initializing the database: %v", err)
 	}
